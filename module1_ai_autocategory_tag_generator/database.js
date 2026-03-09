5206a2e562c863e0c0fb4311bd2cb5d3bfac7e0f
Areesh18 <areeshzafar396@gmail.com>
2026-03-09 16:29:36 +0530

    added a function for getting all prodcuts

--- module1_ai_autocategory_tag_generator/database.js
+++ module1_ai_autocategory_tag_generator/database.js
@@ -1,19 +1,23 @@
-import fs from 'fs';
+import fs from "fs";
 
-const DB_FILE = 'products.json';
+const DB_FILE = "products.json";
 
 export const saveToCatalog = (productData) => {
   let products = [];
 
-  // 1. Read existing products if the file exists
   if (fs.existsSync(DB_FILE)) {
     const rawData = fs.readFileSync(DB_FILE);
     products = JSON.parse(rawData);
   }
 
-  // 2. Add the new AI-generated product
   products.push(productData);
 
-  // 3. Save the updated list back to the file
   fs.writeFileSync(DB_FILE, JSON.stringify(products, null, 2));
+};
+
+
+export const getAllProducts = () => {
+  if (!fs.existsSync(DB_FILE)) return [];
+  const rawData = fs.readFileSync(DB_FILE);
+  return JSON.parse(rawData);
 };
